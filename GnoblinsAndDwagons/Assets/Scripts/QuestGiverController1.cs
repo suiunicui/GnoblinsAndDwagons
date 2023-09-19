c836484bdc05fb843e6a5d6a00baa9503ee0d56e
suiunicui <sunedyrbye@yahoo.dk>
2023-09-19 17:49:44 +0200

    add smith

---
+++ GnoblinsAndDwagons/Assets/Scripts/QuestGiverController1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiverController : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        Debug.Log("You cannot pass! Its not ready yet...");
+    }
+}
