4f346acc3d2e50113df839518bc60b51a0fedcbc
hundahl18 <hundahl18@gmail.com>
2023-10-03 09:27:58 +0200

    Ability to enter shop

--- GnoblinsAndDwagons/Assets/Scripts/MerchantController.cs
+++ GnoblinsAndDwagons/Assets/Scripts/MerchantController.cs
@@ -5,10 +5,14 @@ using ItemThings;
 
 public class MerchantController : MonoBehaviour, Interactable
 {
+    [SerializeField] Dialog dialog;
     [SerializeField] GameStateMemory gameStateMemory;
     public void Interact()
     {
-        
-        
+        gameStateMemory.inDungeon = false;
+        gameStateMemory.inShop = true;
+        gameStateMemory.leaveDungeon = false;
+        gameStateMemory.leaveShop = false;
+        DialogManager.instance.showDialog(dialog, true, "Shop");
     }
 }
